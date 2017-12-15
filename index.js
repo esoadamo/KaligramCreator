7b29e6b01edb3010ede59040a48c98fb438e89e3
Adam Hlaváček <git@adamhlavacek.com>
2017-12-15 17:50:15 +0100

    Editable settings

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const PAPER_RATIO = [1.4, 1];
 const PAPER_WIDTH = 1684; // pixels
-const PAPER_DATA = {'size': [], 'lines': []};  // dictionary with saved data
+const PAPER_DATA = {
+  'size': [],
+  'fontColor': '#000000',
+  'lines': []
+}; // dictionary with saved data
 const COLORS = {
   'paper': "#ffffff",
   'line': "#000000",
@@ -15,8 +19,13 @@ let lineNumberFontSize = 10;
 const FONT_SIZE = 30; // TODO make more scalable
 const FONT = "sans-serif"; // TODO make more adjustable
 
-function repaint(){
-  let text = TXT_AREA.textContent.replace('\n', ' ');  // painted text
+function repaint() {
+  let text = TXT_AREA.value.replace('\n', ' '); // painted text
+  let drawLines = document.querySelector('#drawLines').checked;
+  let drawLineNumbers = document.querySelector('#drawLineNumbers').checked;
+  let drawText = document.querySelector('#drawText').checked;
+  let fontColor = document.querySelector('#textColor').value;
+  PAPER_DATA['fontColor'] = fontColor;
 
   // Paint on white paper
   CTX.fillStyle = COLORS['paper'];
@@ -25,33 +34,38 @@ function repaint(){
   // Paint all lines
   for (let i = 0; i < PAPER_DATA['lines'].length; i++) {
     let line = PAPER_DATA['lines'][i];
-    CTX.fillStyle = COLORS['line'];
-    CTX.beginPath();
-    CTX.moveTo(...line['start']);
-    CTX.lineTo(...line['end']);
-    CTX.closePath();
-    CTX.stroke();
 
-    CTX.font = "30px sans-serif"
-    CTX.fillStyle = COLORS['lineStart'];
-    CTX.fillText(i.toString(), ...line['start']);
-    CTX.fillStyle = COLORS['lineEnd'];
-    CTX.fillText(i.toString(), ...line['end']);
+    if (drawLines) {
+      CTX.fillStyle = COLORS['line'];
+      CTX.beginPath();
+      CTX.moveTo(...line['start']);
+      CTX.lineTo(...line['end']);
+      CTX.closePath();
+      CTX.stroke();
+    }
 
-    if (text.length > 0) {
+    if (drawLineNumbers) {
+      CTX.font = "30px sans-serif"
+      CTX.fillStyle = COLORS['lineStart'];
+      CTX.fillText(i.toString(), ...line['start']);
+      CTX.fillStyle = COLORS['lineEnd'];
+      CTX.fillText(i.toString(), ...line['end']);
+    }
+
+    if (drawText && (text.length > 0)) {
       let textOnThisLine = "";
       let lineWidth = Math.sqrt(Math.pow(Math.abs(line['start'][0] - line['end'][0]), 2), Math.pow(Math.abs(line['start'][1] - line['end'][1]), 2));
+      CTX.fillStyle = fontColor;
       CTX.font = `${FONT_SIZE}px ${FONT}`;
-      while ((CTX.measureText(textOnThisLine).width < lineWidth) && (text.length > 0)){
+      while ((CTX.measureText(textOnThisLine).width < lineWidth) && (text.length > 0)) {
         textOnThisLine += text.substring(0, 1);
         text = text.substring(1);
       }
 
       // We have overflown the length of the line by last character. Don't be mean and return this little fella to the rest of the text.
-      if (CTX.measureText(textOnThisLine).width > lineWidth){
+      if (CTX.measureText(textOnThisLine).width > lineWidth) {
         text = textOnThisLine.substring(textOnThisLine.length - 1) + text;
         textOnThisLine = textOnThisLine.substring(0, textOnThisLine.length - 1);
-        console.log(text);
       }
 
       let lineAngle = Math.atan((line['end'][1] - line['start'][1]) / (line['end'][0] - line['start'][0]));
@@ -65,7 +79,10 @@ function repaint(){
 }
 
 window.onload = () => {
+  // Init global DOM variables
   TXT_AREA = document.querySelector('#paintedText');
+
+  // Set paintingArea size
   let paintingArea = document.querySelector('#paintingArea');
   let paintingAreaWidth = window.innerWidth * 0.8;
   let paintRatio = PAPER_RATIO[0] * PAPER_RATIO[1];
@@ -85,16 +102,26 @@ window.onload = () => {
   paintingArea.addEventListener('click', (e) => {
     let rect = paintingArea.getBoundingClientRect();
     let clickPos = [(e.x - rect.left) * screenRation, (e.y - rect.top) * screenRation];
-    switch(mouseMode){
+    switch (mouseMode) {
       case "prePaint":
         mouseData = clickPos;
         mouseMode = "painting";
         break;
       case "painting":
-        PAPER_DATA['lines'].push({'start': mouseData, 'end': clickPos});
+        PAPER_DATA['lines'].push({
+          'start': mouseData,
+          'end': clickPos
+        });
         mouseData = clickPos;
         repaint();
         break;
     }
   });
+
+  // Add listeners to settings
+  [document.querySelector('#drawLines'), document.querySelector('#drawLineNumbers'), document.querySelector('#drawText'), document.querySelector('#textColor')]
+  .forEach((e) => {
+    e.addEventListener('change', repaint);
+  });
+  TXT_AREA.addEventListener('input', repaint);
 }
